e177b1e15ceb1b6fdd6a08b123ba6fc3c905587c
Ropio-Derrick <rodetuk@gmail.com>
2022-06-27 19:10:26 -0400

    some icons in navbar

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Nav.css'
+import { MiscellaneousServicesSharp,HandshakeTwoTone, InfoSharp } from '@mui/icons-material'
 
 function Nav() {
   return (
@@ -10,9 +11,9 @@ function Nav() {
         </div>
         <div className="Right ">
           <div className="Lister">
-            <a className="Page">Services</a>
-            <a className="Page">Partners</a>
-            <a className="Page">About Us</a>
+            <a className="Page"> <MiscellaneousServicesSharp className="Page-Icon" /> Services</a>
+            <a className="Page"> <HandshakeTwoTone className="Page-Icon" /> Partners</a>
+            <a className="Page"> <InfoSharp  className="Page-Icon" /> About Us</a>
           </div>
           <button className="Join-btn">Join Us</button>
         </div>
